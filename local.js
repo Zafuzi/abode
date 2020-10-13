61f54b0587eac2493b7c81f1215fd131a2c70628
Zachary <zacharyfoutz@gmail.com>
2020-10-13 10:43:50 -0700

    Made it a little interactive
    Former-commit-id: 0070712bdfc09381465a04d9c621113d032f1f00 [formerly 35e15f438641d950a4bf9fcee9942190c6a9f703]
    Former-commit-id: e5321cb8831dee1221f53e21cbfa4e5770b5e087

--- local.js
+++ local.js
@@ -1,28 +1,43 @@
 let Employees = 45;
 
 let Icons = {
+    Credits: "images/wallet.png",
     Steel: "images/steel.png",
     Oxygen: "images/oxygen.png",
     Methane: "images/methane.png",
-    "Methane Farm": "🐄",
-    "Glorified Air Compressor": "☁",
-    "Big Shovel and Stove": "🗻"
+    Rockets: "images/rocket.png"
 }
 
 let Resources = {
+    Credits: 1000000,
     Steel: 4000000,
     Methane: 300000,
-    Oxygen: 1000000
+    Oxygen: 1000000,
+    Rockets: 0
 }
 
 let Structures = [ // rate is x per tick
-    { name: "Big Shovel and Stove", desc: "", resource: "Steel", rate: 100 },
-    { name: "Methane Farm", desc: "", resource: "Methane", rate: 100 },
-    { name: "Glorified Air Compressor", desc: "", resource: "Oxygen", rate: 100 },
+    { name: "Big Shovel and Stove", desc: "", resource: "Steel", rate: 10, count: 1, costs: { Credits: 2000000 }, multiplier: 1, active: true },
+    { name: "Methane Farm", desc: "", resource: "Methane", rate: 1, count: 1, costs: { Credits: 5000000 }, multiplier: 1, active: true },
+    { name: "Glorified Air Compressor", desc: "", resource: "Oxygen", rate: 2, count: 1, costs: { Credits: 3000000 }, multiplier: 1, active: true },
+    {
+        name: "Rocket Fabricator",
+        desc: "",
+        resource: "Rockets",
+        rate: 0,
+        count: 0,
+        active: false,
+        multiplier: 1,
+        costs: {
+            Credits: 750000,
+            Steel: 2000000,
+            Methane: 250000,
+            Oxygen: 800000
+        }
+    },
 ]
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("hello")
     var app = new Vue({
         el: '#app',
         data: {
@@ -30,14 +45,39 @@ document.addEventListener("DOMContentLoaded", () => {
             Structures: Structures,
             Icons: Icons,
             numFmt: numFmt,
+            Tiles: [],
             message: 'Welcome. We have started you off with a few production facilities and we are ready to start constructing our first rocket'
+        },
+        methods: {
+            construct: function(structure, amt) {
+                console.log(structure.costs.Credits * amt);
+                if (structure.costs.Credits * amt <= Resources.Credits) {
+                    structure.count += 1;
+                    structure.rate += .0001;
+                    structure.active = true;
+                    Resources.Credits -= structure.costs.Credits * amt;
+                }
+            }
         }
     })
 
     let loop = function() {
         Structures.forEach(s => {
-            Resources[s.resource] += s.rate;
+            if (s.active) {
+                Object.keys(s.costs).forEach(k => {
+                    let v = s.costs[k];
+                    if (k != "Credits" && Resources[k] >= v * s.rate * s.multiplier) {
+                        Resources[k] -= v * s.rate * s.multiplier;
+                    } else {
+                        s.funding = false;
+                    }
+                })
+                if (!s.funding) {
+                    Resources[s.resource] += s.rate * s.multiplier * s.count;
+                }
+            }
         })
+
         requestAnimationFrame(loop);
     }
 
